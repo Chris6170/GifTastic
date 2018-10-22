c1b7deed036210c12a376c34cfc3689c92fcf947
Fife <39037648+Chris6170@users.noreply.github.com>
2018-10-21 20:04:52 -0700

    final

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -28,12 +28,14 @@ function displayMovieInfo() {
   var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=HUOuIG8fZ9zsGq0poxvJTLT0eujOgUz1&limit=12";
 
 
+
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
 
-    // gifResults.empty();
+    console.log(response);
 
 
 
@@ -41,22 +43,27 @@ function displayMovieInfo() {
 
 
     for (i = 0; i < imgArr; i++) {
-
       var loop = response.data[i].images.original.url;
+      var still = response.data[i].images.original_still.url;
       var rating = response.data[i].rating;
       var favBttn = $("<button>").html("Add to favorites");
-      var imgDiv = $("<div>").addClass(i);
-      var gif = $("<img>").attr('src', loop);
+      var imgDiv = $("<div>");
+      imgDiv.addClass("yes");
+      var gif = $("<img>").attr('src', still);
+      gif.attr('data-state', 'still');
+      gif.attr('data-still', still);
+      gif.attr('data-animate', loop);
+      gif.addClass('gif');
       var giftext = $("<p>").html(rating);
 
-    
-      
+
+
       favBttn.addClass("btn btn-success");
       imgDiv.append(gif);
       imgDiv.append(giftext);
       imgDiv.append(favBttn);
       gifResults.prepend(imgDiv);
-     
+
       console.log(rating);
 
 
@@ -69,14 +76,26 @@ function displayMovieInfo() {
   });
 }
 
-$(document).on("click", ".movie-btn", displayMovieInfo);
+$(document).on('click',".gif", function () {
 
-$(".btn").on('click', function(){
-  
-console.log("yes");
+  console.log("yes");
+
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+
+
+  }
 
 
 
 
-  
+
 })
+
+$(document).on("click", ".movie-btn", displayMovieInfo);
